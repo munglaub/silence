aa3187be5f92addadb8cfc997c1a58a3076901cd
aloeee <aloeee@gmx.de>
2009-07-17 01:18:25 +0200

    cosmetic changes at the about dialog

--- gui/aboutdialog.cpp
+++ gui/aboutdialog.cpp
@@ -1,42 +1,44 @@
 #include "gui/aboutdialog.h"
 #include <QDialog>
-#include <QVBoxLayout>
+#include <QGridLayout>
 #include <QLabel>
 #include <QPushButton>
 
 AboutDialog::AboutDialog(QWidget *parent, Qt::WindowFlags f)
 	: QDialog(parent, f)
 {
-	QLabel *head = new QLabel(tr("Silence very early Version"));
+	setWindowTitle(tr("About Silence"));
+	setFixedSize(350, 175);
+
+	QLabel *head = new QLabel(tr("Silence"));
 	head->setFont(QFont("Times", 18, QFont::Bold));
 
-	QLabel *writtenby = new QLabel(tr("written by"));
-	writtenby->setAlignment(Qt::AlignCenter);
+	QLabel *version = new QLabel(tr("very early Version"));
+	version->setFont(QFont("Times", 12, QFont::Bold));
 
+	QLabel *writtenby = new QLabel(tr("written by"));
 
 	QLabel *author1 = new QLabel(tr("Manuel Unglaub"));
 	author1->setFont(QFont("Times", 12, QFont::Bold));
-	author1->setAlignment(Qt::AlignCenter);
 	QLabel *author2 = new QLabel(tr("Yves Adler"));
 	author2->setFont(QFont("Times", 12, QFont::Bold));
-	author2->setAlignment(Qt::AlignCenter);
 	
-	QPushButton *quit = new QPushButton(tr("Quit"));
+	QPushButton *quit = new QPushButton(tr("Close"));
 	quit->setFixedWidth(100);
 	connect(quit, SIGNAL(clicked()), this, SLOT(accept()));
 
-	layout = new QVBoxLayout();
-	layout->addWidget(head);
-	layout->addWidget(writtenby);
-	layout->addWidget(author1);
-	layout->addWidget(author2);
-	layout->addWidget(quit);
+	QGridLayout *layout = new QGridLayout();
+	layout->addWidget(head, 0, 0, 1, 1, Qt::AlignRight);
+	layout->addWidget(version, 0, 1, 1, 1, Qt::AlignLeft);
+	layout->addWidget(writtenby, 1, 0, 1, 2, Qt::AlignCenter);
+	layout->addWidget(author1, 2, 0, 1, 2, Qt::AlignCenter);
+	layout->addWidget(author2, 3, 0, 1, 2, Qt::AlignCenter);
+	layout->addWidget(quit, 4, 0, 1, 2, Qt::AlignRight);
 	setLayout(layout);
 }
 
 AboutDialog::~AboutDialog()
 {
-	delete layout;
 }
 
 
--- gui/aboutdialog.h
+++ gui/aboutdialog.h
@@ -2,16 +2,12 @@
 #define ABOUTDIALOG_H
 
 #include <QDialog>
-#include <QVBoxLayout>
 
 class AboutDialog : public QDialog
 {
 	public:
 		AboutDialog(QWidget *parent = 0, Qt::WindowFlags f = 0);
 		~AboutDialog();
-	private:
-		QVBoxLayout *layout;
-
 };
 
 #endif // ABOUTDIALOG_H
