6e4633305787be7afc5e9a1411697b44ec99b055
aloeee <aloeee@gmx.de>
2009-07-18 00:32:42 +0200

    some treeview changes

--- gui/treeview.cpp
+++ gui/treeview.cpp
@@ -1,12 +1,22 @@
 #include "gui/treeview.h"
 #include <QDockWidget>
-#include <QTreeView>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QStringList>
 
 TreeView::TreeView(const QString &title, QWidget *parent, Qt::WindowFlags flags)
 	: QDockWidget(title, parent, flags)
 {
 	setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-	QTreeView *tree = new QTreeView();
+	QTreeWidget *tree = new QTreeWidget();
+
+	QTreeWidgetItem *first = new QTreeWidgetItem(tree, 0);
+	first->setText(0, "blah");
+	QTreeWidgetItem *firstsub = new QTreeWidgetItem(first, 0);
+	firstsub->setText(0, "foobar");
+	QTreeWidgetItem *sec = new QTreeWidgetItem(tree, 0);
+	sec->setText(0, "blubb");
+
 	setWidget(tree);
 }
 
