edd45719349acf4ce28573012c2f5507daa66876
Yves Adler <yves.adler@googlemail.com>
2009-07-18 11:07:38 +0200

    verschiedene kleinigkeiten
     	modified:   gui/treeview.cpp
    	modified:   gui/treeview.h

--- gui/treeview.cpp
+++ gui/treeview.cpp
@@ -3,12 +3,24 @@
 #include <QTreeWidget>
 #include <QTreeWidgetItem>
 #include <QStringList>
+#include <QToolBar>
+#include <QVBoxLayout>
+#include <QAction>
+#include <QList>
+#include <QTreeWidgetItem>
 
 TreeView::TreeView(const QString &title, QWidget *parent, Qt::WindowFlags flags)
 	: QDockWidget(title, parent, flags)
 {
 	setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-	QTreeWidget *tree = new QTreeWidget();
+        QToolBar *toolbar = new QToolBar();
+        
+        QAction *addAction = toolbar->addAction("add");        
+        connect(addAction, SIGNAL(triggered()), this, SLOT(addTreeItem()));
+        QAction *removeAction = toolbar->addAction("remove");
+        connect(removeAction, SIGNAL(triggered()), this, SLOT(removeTreeItem()));
+        
+        tree = new QTreeWidget; 
 
 	QTreeWidgetItem *first = new QTreeWidgetItem(tree, 0);
 	first->setText(0, "blah");
@@ -17,10 +29,30 @@ TreeView::TreeView(const QString &title, QWidget *parent, Qt::WindowFlags flags)
 	QTreeWidgetItem *sec = new QTreeWidgetItem(tree, 0);
 	sec->setText(0, "blubb");
 
-	setWidget(tree);
+        QFrame *frame = new QFrame();
+        QVBoxLayout *layout = new QVBoxLayout;  
+        layout->addWidget(toolbar);
+        layout->addWidget(tree);
+        frame->setLayout(layout);
+        setWidget(frame);
 }
 
-TreeView::~TreeView()
+void TreeView::addTreeItem()
 {
+        QTreeWidgetItem *newItem = new QTreeWidgetItem(tree, 0);
+        newItem->setText(0, "testing");
+}
+
+
+void TreeView::removeTreeItem()
+{
+        QTreeWidgetItem *newItem = new QTreeWidgetItem(tree, 0);
+        newItem->setText(0, "ich bin ein fieser segfault :)");
+}
+
+
+TreeView::~TreeView()
+{      
+        delete tree;
 }
 
--- gui/treeview.h
+++ gui/treeview.h
@@ -2,12 +2,22 @@
 #define TREEVIEW_H
 
 #include <QDockWidget>
+#include <QTreeWidget>
 
 class TreeView : public QDockWidget
 {
+        Q_OBJECT
+        
 	public:
 		TreeView(const QString &title, QWidget *parent = 0, Qt::WindowFlags flags = 0);
 		~TreeView();
+                
+        private slots:
+                void addTreeItem();
+                void removeTreeItem();
+                
+        private:
+                 QTreeWidget *tree;
 };
 
 #endif // TREEVIEW_H
