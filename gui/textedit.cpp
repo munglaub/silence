80a8d6064886e08c1b1ea1fb354d37f6e9e0f5e0
aloeee <aloeee@gmx.de>
2009-08-09 17:59:01 +0200

    textedit toolbar

--- gui/textedit.cpp
+++ gui/textedit.cpp
@@ -1,15 +1,41 @@
 #include "textedit.h"
 #include <Qsci/qscilexercpp.h>
+#include <QVBoxLayout>
+#include <QToolBar>
+
 
 TextEdit* TextEdit::textedit = 0;
 
-TextEdit::TextEdit()
+TextEdit::TextEdit(QWidget *parent)
+	: QWidget(parent)
 {
+	// Toolbar
+	QToolBar *toolbar = new QToolBar;
+	toolbar->addAction(tr("save"));
+	toolbar->addSeparator();
+	toolbar->addAction(tr("copy"));
+	toolbar->addAction(tr("cut"));
+	toolbar->addAction(tr("paste"));
+	toolbar->addSeparator();
+	toolbar->addAction(tr("undo"));
+	toolbar->addAction(tr("redo"));
+	toolbar->addSeparator();
+	toolbar->addAction(tr("find"));
+	toolbar->addAction(tr("replace"));
+
+	editor = new QsciScintilla;
+
 	// temporarily with a c++ lexer
-	setLexer(new QsciLexerCPP());
+	editor->setLexer(new QsciLexerCPP());
 	// show linenumbers
-	setMarginWidth(0, 35);
-	setMarginLineNumbers(0, true);
+	editor->setMarginWidth(0, 35);
+	editor->setMarginLineNumbers(0, true);
+
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->setContentsMargins(0, 0, 0, 0);
+	layout->addWidget(toolbar);
+	layout->addWidget(editor);
+	setLayout(layout);
 }
 
 TextEdit* TextEdit::create()
@@ -19,8 +45,15 @@ TextEdit* TextEdit::create()
 	return textedit;
 }
 
+void TextEdit::setText(const QString &text)
+{
+	editor->setText(text);
+}
+
+
 TextEdit::~TextEdit()
 {
+	delete editor;
 }
 
 
--- gui/textedit.h
+++ gui/textedit.h
@@ -2,18 +2,22 @@
 #define TEXTEDIT_H
 
 #include <Qsci/qsciscintilla.h>
+#include <QWidget>
 
-class TextEdit : public QsciScintilla
+
+class TextEdit : public QWidget
 {
 	public:
 		~TextEdit();
 		static TextEdit* create();
+		void setText(const QString &text);
 
 	protected:
-		TextEdit();
+		TextEdit(QWidget *parent = 0);
 	
 	private:
 		static TextEdit *textedit;
+		QsciScintilla *editor;
 };
 
 #endif // TEXTEDIT_H
