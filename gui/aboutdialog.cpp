f79c58e251a6d38fef1f80c79acc827e3502f6ef
Manuel Unglaub <m.unglaub@gmail.com>
2009-09-05 13:10:42 +0200

    license

--- gui/aboutdialog.cpp
+++ gui/aboutdialog.cpp
@@ -1,3 +1,23 @@
+/*
+ * Silence
+ *
+ * Copyright (C) 2009 Manuel Unglaub
+ *
+ * This file is part of Silence.
+ *
+ * Silence is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version GPLv2 only of the License.
+ *
+ * Silence is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Silence.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "gui/aboutdialog.h"
 
 AboutDialog::AboutDialog(QWidget *parent, Qt::WindowFlags f)
