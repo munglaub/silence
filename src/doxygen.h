cf5cde8cbfcd59ead20d54444bf60a3afb221e85
Manuel Unglaub <m.unglaub@gmail.com>
2010-05-14 19:07:31 +0200

    documentation mainpage text

---
+++ src/doxygen.h
@@ -0,0 +1,8 @@
+/*!
+ * \mainpage Silence Documentation
+ *
+ * This documentation is for developers interested in the sourcecode of Silence.
+ * It might not be complet and could certainly be improved but maybe it can help to get familiar with the Silence sourcecode.
+ * To get a more up-to-date version of this documentation you can checkout Silence form the gitorious repository and generate it at your own machine.
+ *
+ */
